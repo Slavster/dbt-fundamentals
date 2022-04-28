68b4cf1221bf1857046505ca7e0ab5691e3e2825
Slav Sinitsyn <Slav.Sinitsyn@gmail.com>
2022-04-28 20:28:20 +0000

    feat: updating fact_customer_orders_v2 to use windowing functions

--- models/marts/fact_customer_orders_v2.sql
+++ models/marts/fact_customer_orders_v2.sql
@@ -19,10 +19,14 @@ select
     -- indicated paid_orders
     ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
     ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-    CASE WHEN co.first_order_date = p.order_placed_at
-        THEN 'new' ELSE 'return' END as nvsr,
-    --order_clv as customer_lifetime_value,
-    co.first_order_date as fdos
+    CASE WHEN ( rank() over (
+        partition by customer_id
+        order by order_placed_at, order_id ) = 1
+    ) THEN 'new' ELSE 'return' END as nvsr,
+    first_value(order_placed_at) over (
+        partition by customer_id
+        order by order_placed_at
+    ) as fdos
 FROM paid_orders p
 left join customer_orders as co USING (customer_id)
 ORDER BY order_id
